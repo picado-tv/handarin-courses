37cf2c4f63072b62d12eb40ace974f1b4e0be7e5
rockpicado <rock.picado@gmail.com>
2023-11-25 21:16:26 +0900

    Override page urls

--- site.config.ts
+++ site.config.ts
@@ -42,11 +42,11 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/무료-강좌': '0ae92e7bfd504543bc5d30309f6ceb35',
+    '/연락처': '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
@@ -58,7 +58,7 @@ export default siteConfig({
       pageId: '0ae92e7bfd504543bc5d30309f6ceb35'
     },
     {
-      title: 'Contact',
+      title: '연락처',
       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
     }
   ]
