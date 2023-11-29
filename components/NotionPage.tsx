7c5acea247b0d4c00f30423ae8a33110a6889dc8
rockpicado <rock.picado@gmail.com>
2023-11-29 14:57:16 +0900

    relocate gtm tag location

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -255,10 +255,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
       {isLiteMode && <BodyClassName className='notion-lite' />}
       {isDarkMode && <BodyClassName className='dark-mode' />}
-      {/* <!-- Google Tag Manager (noscript) --> */}
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWK2S6J6"
-      height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
-      {/* <!-- End Google Tag Manager (noscript) --> */}
 
       <NotionRenderer
         bodyClassName={cs(
